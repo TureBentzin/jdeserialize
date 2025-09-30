1bec7d7d78c3b361a970db383cd2b9931d9e33e5
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-09-30 06:53:09 +0000

    update: add junit

--- flake.nix
+++ flake.nix
@@ -15,11 +15,42 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        jdk = pkgs.jdk21;
+        junit =
+          let
+            pname = "junit-platform-console-standalone";
+            version = "6.0.0-RC2";
+            hash = "sha256-Ecoot6gbs+gSZOZrS+8dYZSBizPjurAKcoEAR6qg/pA=";
+          in
+          pkgs.stdenv.mkDerivation {
+            inherit pname version;
+
+            src = pkgs.fetchurl {
+              url = "https://repo1.maven.org/maven2/org/junit/platform/${pname}/${version}/${pname}-${version}.jar";
+              inherit hash;
+            };
+
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
+            unpackPhase = "true";
+
+            installPhase = ''
+              mkdir -p $out/lib
+              cp $src $out/lib/$pname.jar
+
+              mkdir -p $out/bin
+              makeWrapper ${jdk}/bin/java $out/bin/junit \
+                --add-flags "-jar $out/lib/$pname.jar"
+            '';
+          };
+
         packages = [
           pkgs.ant
-          pkgs.jdk21
+          jdk
+          junit
           pkgs.makeWrapper
         ];
+
       in
       {
         devShells.default = pkgs.mkShell {
@@ -37,18 +68,18 @@
             buildInputs = [ ];
             nativeBuildInputs = packages;
             buildPhase = ''
-            ant
-            ant javadoc
+              ant
+              ant javadoc
             '';
             installPhase = ''
-            mkdir -p $out/{bin,lib}
-            mkdir -p $out/share/{doc,licenses}/jdeserialize/
+              mkdir -p $out/{bin,lib}
+              mkdir -p $out/share/{doc,licenses}/jdeserialize/
 
-            cp jdeserialize.jar $out/lib/
-            cp -r javadoc $out/share/doc/jdeserialize/javadoc
-            cp ORIGINAL-LICENSE.md $out/share/licenses/jdeserialize/
+              cp jdeserialize.jar $out/lib/
+              cp -r javadoc $out/share/doc/jdeserialize/javadoc
+              cp ORIGINAL-LICENSE.md $out/share/licenses/jdeserialize/
 
-            makeWrapper ${jre}/bin/java $out/bin/jdeserialize --add-flags "-jar $out/lib/jdeserialize.jar"
+              makeWrapper ${jre}/bin/java $out/bin/jdeserialize --add-flags "-jar $out/lib/jdeserialize.jar"
             '';
             # meta = {};
           };
